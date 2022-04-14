6138bbcc960a9626d2a46a89fa4348ccbdb1abd6
Daniel Alves <dalima.dev@gmail.com>
2022-04-14 10:25:53 -0300

    feat

--- backend/src/routes/medics.route.js
+++ backend/src/routes/medics.route.js
@@ -3,6 +3,7 @@ const medicsController = require('../controllers/medics.controller');
 
 router.get('/find-medics', medicsController.findMedicsController);
 router.get('/find-medics/:id', medicsController.findMedicByIdController);
+router.get('/details/:id', () => {}) //For later use.
 router.post('/create', medicsController.createMedicController);
 router.put('/update/:id', medicsController.updateMedicController);
 router.delete('/delete/:id', medicsController.deleteMedicController);
