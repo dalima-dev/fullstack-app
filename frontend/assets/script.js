a2221076e67dda6e9f51a15f3fc9fd74a0beb1ed
Daniel Alves <dalima.dev@gmail.com>
2022-04-17 23:03:34 -0300

    feat

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,25 +1,23 @@
 const address = '192.168.0.6'; //You may change this to localhost or 127.0.0.1 if you're not using a wi-fi connection.
 const baseURL = `http://${address}:3000/medic`;
 
-(async () => {
+(async function() {
   //Here we push our medic list data into HTML.
   const response = await fetch(`${baseURL}/find-medics`);
   const medics = await response.json();
 
-  medics.forEach((item) => {
+  medics.forEach(function(item) {
     document.querySelector('#medicList').insertAdjacentHTML(
       'beforeend',
       `
-       <div onclick="findMedicById(${item.id})" class="flex flex-col items-center gap-4 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80 transition delay-300 duration-300 ease-in-out hover:scale-105 cursor-pointer">
+       <div onclick="findMedicById(${item.id}); openModalDetails();" class="flex flex-col items-center gap-4 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80 transition delay-300 duration-300 ease-in-out hover:scale-105 cursor-pointer">
           <img
             src="./assets/foto.jpg"
             alt="image not loaded"
-            class="rounded"
           />
           <div>
             <p>ID: ${item.id}</p>
-            <p>Nome:</p>
-            <p>${item.name}</p>
+            <p>Nome: ${item.name}</p>
           </div>
         </div>
        `,
@@ -28,70 +26,90 @@ const baseURL = `http://${address}:3000/medic`;
 })();
 
 async function findMedicById(idMedic) {
-  const response = await fetch(
-    `${baseURL}/find-medics/${idMedic}`,
-  );
+  const response = await fetch(`${baseURL}/find-medics/${idMedic}`);
   const medic = await response.json();
   const chosenMedicDiv = document.querySelector('#chosenMedic');
 
+  let specialties = '';
+  medic.specialties.forEach(function(specialty) {
+    specialties += `<p>${specialty}</p>`;
+  });
+
   chosenMedicDiv.innerHTML = `
-  <div class="flex flex-row items-center gap-4 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80 cursor-pointer">
+  <div id="modalDetails" class="flex z-[9999] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex-col justify-center gap-2 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80">
+      <div class="flex justify-end"><a
+        onclick="closeModalDetails()"
+        class="mx-2 text-red-500 text-2xl transition-all ease-out duration-150 hover:scale-150 cursor-pointer"
+      > x</a></div>
       <img
         src="./assets/foto.jpg"
         alt="image not loaded"
-        class=" w-20 md:w-auto rounded"
+        class="object-cover w-60"
       />
     
     <div>
-        <p>ID: ${medic.id}</p>
-        <p>Nome:</p>
-        <p>${medic.name}</p>
+        <p>Name: ${medic.name}</p>
+        <p>CRM: ${medic.CRM}</p>
+        <p>Landline: ${medic.landline}</p>
+        <p>Phone Number: ${medic.phoneNumber}</p>
+        <p>CEP: ${medic.CEP}</p>
+        <p>Specialties:</p>
+        ${specialties}
     </div>
   </div>
   `;
 }
 
+function openModalDetails() {
+  document.querySelector('#medicList').style.filter = 'blur(24px)';
+}
+
+function closeModalDetails() {
+  document.querySelector('#modalDetails').style.display = 'none';
+  document.querySelector('#medicList').style.filter = 'blur(0)';
+}
+
 function closeModalRegister() {
   document.querySelector('#overlay').style.display = 'none';
   document.querySelector('#medicList').style.filter = 'blur(0)';
-  document.querySelector('#searchById').style.filter = 'blur(0)';
-  document.querySelector('h2').style.filter = 'blur(0)';
 
   document.querySelector('#name').value = '';
   document.querySelector('#crm').value = '';
   document.querySelector('#landline').value = '';
   document.querySelector('#phoneNumber').value = '';
   document.querySelector('#cep').value = '';
-  document.querySelector('#specialtiesNumber').value = '';
-  document.querySelector('#specialtiesInputs').innerHTML = '';
+  document.querySelector('input[name=alergology]').checked = false
+  document.querySelector('input[name=angiology]').checked = false
+  document.querySelector('input[name=bucoMaxillo]').checked = false
+  document.querySelector('input[name=clinicCardiology]').checked = false
+  document.querySelector('input[name=childrensCardiology]').checked = false
+  document.querySelector('input[name=headNeckSurgery]').checked = false
+  document.querySelector('input[name=cardiacSurgery]').checked = false
+  document.querySelector('input[name=chestSurgery]').checked = false
 }
 
 function openModalRegister() {
   document.querySelector('#overlay').style.display = 'block';
   document.querySelector('#medicList').style.filter = 'blur(24px)';
-  document.querySelector('#searchById').style.filter = 'blur(24px)';
-  document.querySelector('h2').style.filter = 'blur(24px)';
 }
 
-function addSpecialtiesInputs() {
-  const specialtiesNumber = document.querySelector('#specialtiesNumber').value;
+  let specialties = [];
+  let alergologyChecked = false;
+  let angiologyChecked = false;
+  let bucoMaxilloChecked = false;
+  let clinicCardiologyChecked = false;
+  let childrensCardiologyChecked = false;
+  let headNeckSurgeryChecked = false;
+  let chestSurgeryChecked = false;
 
-  if (specialtiesNumber > 12) return; //Maximum number of 12 specialties.
+  const alergology = document.querySelector('input[name=alergology]')
+  alergology.addEventListener('change', function(){
+   console.log(alergology.checked) 
+  })
 
-  const specialtiesInputsDiv = document.querySelector('#specialtiesInputs');
-  specialtiesInputsDiv.innerHTML = '';
 
-  for (let i = 0; i < specialtiesNumber; i++)
-    specialtiesInputsDiv.innerHTML += `
-    <input
-      class="p-1 m-1 text-black border-2 border-slate-600"
-      type="text"
-      id="specialty${i + 1}"
-      name="Specialty"
-      placeholder="Specialty ${i + 1}"
-    />
-    `;
-}
+  //specialties.push(document.querySelector(`#specialty${i + 1}`).value);
+
 
 async function registerMedic() {
   const name = document.querySelector('#name').value;
@@ -99,11 +117,7 @@ async function registerMedic() {
   const landline = document.querySelector('#landline').value;
   const phoneNumber = document.querySelector('#phoneNumber').value;
   const CEP = document.querySelector('#cep').value;
-  const specialtiesNumber = document.querySelector('#specialtiesNumber').value;
-  const specialties = [];
-
-  for (let i = 0; i < specialtiesNumber; i++)
-    specialties.push(document.querySelector(`#specialty${i + 1}`).value);
+  
 
   const medic = { name, CRM, landline, phoneNumber, CEP, specialties };
 
@@ -118,11 +132,12 @@ async function registerMedic() {
 
   const newMedic = await response.json();
 
-  if (response.status != 400) //This prevents of invalid data be shown on screen.
+  if (response.status != 400)
+    //This if statement prevents of invalid data be shown on screen.
     document.querySelector('#medicList').insertAdjacentHTML(
       'beforeend',
       `
-  <div class="flex flex-col items-center gap-4 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80 transition delay-300 duration-300 ease-in-out hover:scale-105 cursor-pointer">
+  <div onclick="findMedicById(${newMedic.id}); openModalDetails();" class="flex flex-col items-center gap-4 p-2 rounded bg-blue-500 shadow-lg shadow-blue-500/80 transition delay-300 duration-300 ease-in-out hover:scale-105 cursor-pointer">
         <img
           src="./assets/foto.jpg"
           alt="image not loaded"
@@ -130,8 +145,7 @@ async function registerMedic() {
         />
         <div>
           <p>ID: ${newMedic.id}</p>
-          <p>Nome:</p>
-          <p>${newMedic.name}</p>
+          <p>Nome: ${newMedic.name}</p>
         </div>
   </div>
   `,
